86968672abefc93c07eb63a21d5df53f29898b70
rkskekzzz <wkdlfflxh@naver.com>
2022-05-16 13:00:17 +0900

    :art: Dummy 구조 개선 * Dummy value * Global Define value

---
+++ config/include/Defines.hpp
@@ -0,0 +1,11 @@
+#ifndef DUMMY_HPP
+#define DUMMY_HPP
+
+#define DEFAULT_CONFIG_PATH		"./config_example.config"
+
+#define SERVER_BLOCK_OPEN		"server {"
+#define SERVER_BLOCK_CLOSE		"}"
+#define LOCATION_BLOCK_OPEN		"\tlocation"
+#define LOCATION_BLOCK_CLOSE	"\t}"
+
+#endif
--- config/include/Dummy.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef DUMMY_HPP
-#define DUMMY_HPP
-
-#define DEFAULT_SERVER_BLOCK(A) (std::vector<std::string> vect(A))
-#define DEFAULT_CONFIG_PATH "./config_example.config"
-#endif
