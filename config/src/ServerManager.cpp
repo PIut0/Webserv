83b453cf4229b955b162d4fca66ee0608e7af3fa
rkskekzzz <wkdlfflxh@naver.com>
2022-05-17 16:59:07 +0900

    :art: log print 위치 수정

--- config/src/ServerManager.cpp
+++ config/src/ServerManager.cpp
@@ -30,7 +30,6 @@ void ServerManager::init_server(std::string path)
             this->getMonitor().print(COLOR_GREEN, "new server block open");
             state <<= 1;
         } else if (state & S_SERVER && line == SERVER_BLOCK_CLOSE) {
-            this->getMonitor().print(COLOR_RED, "server block close");
             this->server.push_back(ServerBlock(temp));
             CLEAR_VECTOR_COMPLETLY(temp)
             state >>= 1;
@@ -38,6 +37,7 @@ void ServerManager::init_server(std::string path)
             temp.push_back(line);
         }
     }
+    this->getMonitor().print(COLOR_RED, "server block close");
 }
 
 
