b3b5fce524d93c4a68901cb398ad053475dbc65b
rkskekzzz <wkdlfflxh@naver.com>
2022-05-22 17:59:52 +0900

    :bulb: 테스트 주석 추가

--- config/src/main.cpp
+++ config/src/main.cpp
@@ -6,10 +6,13 @@
 
 
 int main(int argc, char** argv) {
-	// if (!check_arg(argc, argv)) {
+	// if (check_arg(argc, argv)) {
 	// 	std::cerr << "error" << std::endl;
 	// 	exit(1);
 	// }
 	ServerManager serverManager(check_arg(argc, argv));
+	// std::vector<std::string> str = string_split("hi hello world", " ", 0);
+	// for (size_t i = 0 ; i < str.size() ; i++)
+	// 	std::cout << str[i] << std::endl;
 	return (0);
 }
