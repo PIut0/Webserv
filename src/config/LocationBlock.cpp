68f616b93162be31f7215006bdc80cdb161933a9
Sehyeon An <sehyan@c3r5s3.42seoul.kr>
2022-06-12 15:30:28 +0900

    Fix: 주석삭제

--- src/config/LocationBlock.cpp
+++ src/config/LocationBlock.cpp
@@ -40,7 +40,6 @@ LocationBlock& LocationBlock::operator=(const LocationBlock& rv)
 void LocationBlock::ParseRoot(const std::string &data)
 {
   this->root = data;
-  // TODO  root 가 디렉토리가 아니라 파일인 경우가 있는듯
   if (IsRegularFile(data) < 0) {
     ExitWithMsg("Root Path Error");
   }
